6f907ec54e9ca9fac8db769b30ebf17bfd8af9c1
Adryan Eka Vandra <adryanekavandra@gmail.com>
2023-03-25 15:25:47 +0700

    Initial Commit

---
+++ iNewsOs/iNewsOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNewsOsApp.swift
+//  iNewsOs
+//
+//  Created by Adryan Eka Vandra on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iNewsOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
